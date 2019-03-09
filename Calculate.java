040ce197f3d53fc9820f31ae0319b4585bfc3d96
Markyd <kjiuhuka@mail.ru>
2019-03-09 10:51:31 +0500

    Calculator

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,9 @@ public class Calculate {
 
     public static void main(String[] arg) {
         System.out.println("Calculate");
-
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        double summ = Math.pow(first, second);
+        System.out.println("summ " + summ);
     }
 }
